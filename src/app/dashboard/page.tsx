ee5176052939d694c8b97cda8ca8d89dfed99191
felipe <felipe_lima10@icloud.com>
2023-05-09 15:47:17 -0300

    feat page dashboard created

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+
+export default async function Dashboard() {
+
+  const session = await getServerSession(authOptions)
+
+  return (
+    <pre>{JSON.stringify(session)}</pre>
+  )
+}
